27ab9aadd52fa589048b4ae426ff99ff7ac866ba
codeofduty <partners@codeofduty.dev>
2021-06-03 17:21:57 +0200

    🎉

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+      <header>
+        <h1>CodeofDuty:  React-scroll-Observable</h1>
+      </header>
+      <main>
+
+      </main>
+    </>
+  )
+}
+
+export default App
